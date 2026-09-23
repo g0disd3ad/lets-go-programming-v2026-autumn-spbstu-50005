fae82bb9e1cc038f698d15eda217793c634e54d2
Melnikova Anna <melnikova2.am@edu.spbstu.ru>
2026-09-23 15:09:16 +0300

    [TASK-1] Initial commit

---
+++ anna.melnikova/task-1/cmd/service/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int
+	var c string
+	_, err := fmt.Scan(&a)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+	_, err = fmt.Scan(&b)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+	_, err = fmt.Scan(&c)
+	if err != nil || (c != "+" && c != "-" && c != "*" && c != "/") {
+		fmt.Println("Invalid operation")
+		return
+	}
+	if c == "/" && b == 0 {
+		fmt.Println("Division by zero")
+		return
+	}
+	if c == "-" {
+		fmt.Println(a - b)
+		return
+	} else if c == "*" {
+		fmt.Println(a * b)
+		return
+	} else if c == "/" {
+		fmt.Println(a / b)
+		return
+	} else {
+		fmt.Println(a + b)
+		return
+	}
+
+}
